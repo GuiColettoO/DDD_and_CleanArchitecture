afdcd0fa0497c695be4f35b9762055b9e491fad5
GUILHERME COLETTO DE OLIVEIRA <RM93818@fiap.com.br>
2024-03-08 22:07:29 -0300

    Desafio 3 Notification

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -5,19 +5,31 @@ describe("Product unit tests", () => {
   it ("should throw error when id is empty", () => {
     expect(() => {
       let order = new Product("", "Product 1", 100);
-    }).toThrowError('ID is required');
+    }).toThrowError('product: Id is required');
   })
 
-  it ("should throw error when name is empty", () => {
+  it ("should throw error when id is empty", () => {
     expect(() => {
       let order = new Product("1", "", 100);
-    }).toThrowError('Name is required');
+    }).toThrowError('product: Name is required');
   })
 
   it ("should throw error when price is less then zero", () => {
     expect(() => {
       let order = new Product("1", "Product 1", -1);
-    }).toThrowError('Price must be greater than zero');
+    }).toThrowError('product: Price must be greater than 0');
+  })
+
+  it ("should throw error when id and name is empty", () => {
+    expect(() => {
+      let order = new Product("", "", 100);
+    }).toThrowError('product: Id is required, product: Name is required');
+  })
+
+  it ("should throw error when id, name and price is empty", () => {
+    expect(() => {
+      let order = new Product("", "", -1);
+    }).toThrowError('product: Id is required, product: Name is required, product: Price must be greater than 0');
   })
 
   it ("should change name", () => {
